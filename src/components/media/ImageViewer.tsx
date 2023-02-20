90708c312cd7bab216488c70b3f529a24d99e037
Leland Reardon <leland6925@gmail.com>
2023-02-21 00:46:17 +0800

    info box

--- src/components/media/ImageViewer.tsx
+++ src/components/media/ImageViewer.tsx
@@ -1,18 +1,49 @@
 import React from 'react'
+import { BsInfoSquare } from 'react-icons/bs'
+import { Popover } from 'antd'
 
 interface ImageViewerProps {
     src: string;
     alt: string;
     caption?: string;
+    metadata?: {
+        iso?: number;
+        ss?: number;
+        f?: number;
+        lens?: string;
+        body?: string;
+    }
 }
 
 export default function ImageViewer(props: ImageViewerProps) {
+
+    const Metadata = props.metadata ? 
+        <div style={{backgroundColor: '#151515'}}>
+            {Object.entries(props.metadata).map(([k, v]) => {
+                return (
+                    <p className="text-white">
+                        <em>{k}</em>: <b>{v}</b>
+                    </p>
+                )
+            })}
+        </div> : null
+    
     return (
         <div className='image-viewer'>
             <div className='image-container'>
                 <img src={props.src} alt={props.alt ?? ""}/>
             </div>
             {props.caption ? <div><p className='caption text-white italic'>{props.caption}</p></div> : null}
+            {props.metadata ?
+            <Popover
+                placement="topLeft"
+                arrow={false}
+                content={Metadata}
+                overlayInnerStyle={{backgroundColor: '#151515'}}
+            >
+                <BsInfoSquare color='white'/>
+            </Popover>
+            : null}
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,37 +19,44 @@ export default function Home() {
             src="https://storage.googleapis.com/portfolio-photos/tarsier-close-up.jpg"
             alt="Tarsier Close Up"
             caption="Tarsiers sleep with their eyes open, which enables them to interface directly with the physical underpinnings of the universe."
-          />
+            metadata={{f: 2.8, lens: 'Sony FE 24-70mm F2.8 GM'}}
+/>
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/surf-shop-sunset.jpg"
             alt="Surf Shop Sunset"
             caption="Surfline says it's orange!"
+            metadata={{f: 2.8, lens: 'Sony FE 24-70mm F2.8 GM'}}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/renee-eye.jpg"
             alt="Eye Close Up"
             caption="Hoi hoi"
+            metadata={{f: 2.8, lens: 'Sony FE 24-70mm F2.8 GM'}}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/bird-watch-lr.jpg"
             alt="Bird Watch"
             caption="Bird watching"
+            // metadata={{Body: 'Sony a7Riii'}}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/lunchtime-lr.jpg"
             alt="Lunchtime"
             caption="I made a new friend today, he invited me for lunch."
-          />
+            // metadata={{Body: 'Sony a7Riii'}}
+/>
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/family-time-lr.jpg"
             alt="Family Time"
             caption="Don't mess with Mama"
-          />
+            // metadata={{Body: 'Sony a7Riii'}}
+/>
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/walking-home-alone-lr.jpg"
             alt="Walking Home Alone"
             caption="Walking far from home (See artist Iron and Wine)"
-          />
+            // metadata={{body: 'Sony a7Riii'}}
+/>
         </Gallery>
       </div>
     </>
