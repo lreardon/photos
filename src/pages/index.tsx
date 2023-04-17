a75369c8d99d6c4fbf37d105be53dfca5fcec3f1
Leland Reardon <leland6925@gmail.com>
2023-04-17 10:24:57 +0545

    added a photo and metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,49 +19,55 @@ export default function Home() {
             src="https://storage.googleapis.com/portfolio-photos/tarsier-close-up.jpg"
             alt="Tarsier Close Up"
             caption="Tarsiers sleep with their eyes open, which enables them to interface directly with the physical underpinnings of the universe."
-            metadata={{f: 2.8, lens: 'Sony FE 24-70mm F2.8 GM'}}
+            metadata={{lens: 'Sony FE 24-70mm F2.8 GM', ss: '1/320', f: 2.8, iso: 500, mm: 70 }}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/surf-shop-sunset.jpg"
             alt="Surf Shop Sunset"
             caption="Surfline says it's orange!"
-            metadata={{f: 2.8, lens: 'Sony FE 24-70mm F2.8 GM'}}
+            metadata={{lens: 'Sony FE 24-70mm F2.8 GM', ss: '1/100', f: 2.8, iso: 4000, mm: 32 }}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/renee-eye.jpg"
             alt="Eye Close Up"
             caption="Hoi hoi"
-            metadata={{f: 2.8, lens: 'Sony FE 24-70mm F2.8 GM'}}
+            metadata={{lens: 'Sony FE 24-70mm F2.8 GM', ss: '1/1000', f: 2.8, iso: 100, mm: 70 }}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/bird-watch-lr.jpg"
             alt="Bird Watch"
             caption="Juvenile bald eagles chill in a tree, keeping an eye out for food and girls."
-            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD'}}
+            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', ss: '1/320', f: 6.3, iso: 100, mm: 300 }}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/lunchtime-lr.jpg"
             alt="Lunchtime"
             caption="I made a new friend today, he invited me for lunch."
-            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD'}}
+            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', ss: '1/320', f: 6.3, iso: 8000, mm: 300 }}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/family-time-lr.jpg"
             alt="Family Time"
             caption="A mother Kodiak brown bear naps on the beach with her three cubs (a third is hidden behind her)."
-            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD'}}
+            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', ss: '1/400', f: 6.3, iso: 800, mm: 300 }}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/walking-home-alone-lr.jpg"
             alt="Walking Home Alone"
             caption="Walking far from home (See artist Iron and Wine)"
-            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD'}}
+            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', ss: '1/400', f: 6.3, iso: 800, mm: 300 }}
           />
           <ImageViewer
             src="https://storage.googleapis.com/portfolio-photos/puffin-portrait-foot.jpg"
             alt="Puffin Portrait"
             caption="A puffin floats in front of our boat, but just a little too far away to get a clear shot."
-            metadata={{ss: '1/1250' ,f: 6.3, lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', }}
+            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', ss: '1/1250', f: 6.3, iso: 320, mm: 300  }}
+          />
+          <ImageViewer
+            src="https://storage.googleapis.com/portfolio-photos/spotted-deer-pair.jpg"
+            alt="A Pair of Spotted Deer"
+            caption="Can you spot the other one? Spot! Get it?"
+            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', ss: '1/1250', f: 6.3, iso: 500, mm: 300 }}
           />
         </Gallery>
       </div>
