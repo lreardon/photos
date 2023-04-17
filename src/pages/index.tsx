5deffe240e4c4197cf08d186ab949d8aac9afac5
Leland Reardon <leland6925@gmail.com>
2023-04-17 10:28:07 +0545

    added leopard photo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,6 +69,12 @@ export default function Home() {
             caption="Can you spot the other one? Spot! Get it?"
             metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', ss: '1/1250', f: 6.3, iso: 500, mm: 300 }}
           />
+          <ImageViewer
+            src="https://storage.googleapis.com/portfolio-photos/leopard-far.jpg"
+            alt="Leopard in Yala National Park"
+            caption="Lucas the leopard makes his afternoon rounds. Maybe a bit too dark, still."
+            metadata={{lens: 'Tamron A047 70-300mm F/4.5-6.3 Di III RXD', ss: '1/1250', f: 6.3, iso: 500, mm: 261 }}
+          />
         </Gallery>
       </div>
     </>
